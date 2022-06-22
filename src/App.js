c390665ecfa71389faedd475ad56117ef84aef4e
AndYuz27 <yuzlov2003@gmail.com>
2022-06-22 13:36:54 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Suspense } from 'react';
 
 function App() {
+  // const { fetchAllMessages } = useActions()
+  // React.useEffect(() => {
+  //     fetchAllMessages()
+  // }, [])
+  const AppWrpd = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: stretch;
+    flex-direction: column;
+    width: 100%;
+    min-height: 100vh;
+    background: ${(props) => props.background || 'white'};
+
+`
   return (
     <div className="App">
+    
+
+
+    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
